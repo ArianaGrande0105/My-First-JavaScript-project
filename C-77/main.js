167a0d5f7fb320bdbd7da92258471b3238a2c579
ArianaGrande0105 <81506507+ArianaGrande0105@users.noreply.github.com>
2021-10-05 18:35:00 +0530

    Add files via upload

---
+++ C-77/main.js
@@ -0,0 +1,20 @@
+console.log("Hi I'm Yashika");
+var name1 ="Yashika ";
+console.log (name1);
+var name2 = "Soni";
+console.log (name2);
+var fullname = name1.concat(name2);
+console.log (fullname);
+console.log (fullname.length);
+var no1 = 6;
+console.log (no1)
+var no2 = 50
+console.log (no2)
+var sum = no1 + no2
+console.log (sum)
+var subtract = no1 - no2
+console.log (subtract)
+var multiply = no1 * no2
+console.log (multiply)
+var division = no1 / no2
+console.log (division)
